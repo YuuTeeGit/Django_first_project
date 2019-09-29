3a12bfcb65d7b673cef91e0aad13427654695ec3
YuuTeeGit <soro.2713@gmail.com>
2019-09-29 18:13:23 +0900

    commit

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
     #return HttpResponse('Hello World!!')
     my_dict = {'insert_me':'Hello I am from first_app/views.py!'}
